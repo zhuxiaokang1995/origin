e540ed4daf9766b1b6d7edc1b57436750822eaaf
YXQ <yuxianquan@shmingjiang.com.cn>
2018-03-29 14:03:20 +0800

    添加TransportTask接口逻辑

--- src/main/java/com/mj/holley/ims/repository/TransportTaskRepository.java
+++ src/main/java/com/mj/holley/ims/repository/TransportTaskRepository.java
@@ -1,15 +1,26 @@
 package com.mj.holley.ims.repository;
 
 import com.mj.holley.ims.domain.TransportTask;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-import org.springframework.data.jpa.repository.*;
-
-import java.util.List;
+import java.time.ZonedDateTime;
 
 /**
  * Spring Data JPA repository for the TransportTask entity.
  */
 @SuppressWarnings("unused")
 public interface TransportTaskRepository extends JpaRepository<TransportTask,Long> {
+
+    TransportTask findByTaskID(Integer taskId);
+
+    @Modifying
+    @Query(value = "update transport_task t set t.fun_id = ?1 , t.serial_id = ?2 , t.task_type = ?3 , t.task_prty = ?4 ,\n" +
+        "t.task_flag = ?5 , t.l_pn = ?6 , t.frm_pos = ?7 , t.frm_pos_type = ?8 , t.to_pos = ?9 , t.to_pos_type = ?10 ,\n" +
+        "t.op_flag = ?11 , t.remark = ?12 , t.issued_task_time = ?13  where t.task_id = ?14", nativeQuery = true)
+    int updateTransportTask(String funId , Integer serialId , String taskType , String taskPrty , String taskFlag , String lpn ,
+                            String frmPos , String frmPosType , String toPos , String toPosType , String opFlag , String remark ,
+                            ZonedDateTime issuedTaskTime , Integer taskId);
 
 }
